afef37a338eb484d6f0f29c86395bdcc151d8945
fidelver <49191506+fidelver@users.noreply.github.com>
2025-07-31 01:00:41 -0600

    Update climate_ir_aeha.h

--- esphome/components/climate_ir_aeha/climate_ir_aeha.h
+++ esphome/components/climate_ir_aeha/climate_ir_aeha.h
@@ -1,23 +1,16 @@
 #pragma once
-#include "esphome/components/climate/climate.h"
-#include "esphome/components/remote_base/remote_base.h"
+#include "esphome/components/climate/climate_ir.h"
 
 namespace esphome {
 namespace climate_ir_aeha {
 
-class AehaClimate : public climate::Climate, public Component {
+class AehaClimate : public climate::ClimateIR {
  public:
-  void setup() override;
-  void control(const climate::ClimateCall &call) override;
-  void set_transmitter(remote_base::RemoteTransmitter *transmitter) { transmitter_ = transmitter; }
+  void transmit_state() override;
   void set_address(uint16_t address) { address_ = address; }
-
+  
  protected:
-  void transmit_state_();
-  uint8_t calculate_checksum_(const uint8_t *data, size_t length);
-  remote_base::RemoteTransmitter *transmitter_;
-  uint16_t address_ = 0xC4D3;  // Default LG AEHA address
-  bool turbo_ = false;
+  uint16_t address_ = 0xC4D3;
 };
 
 }  // namespace climate_ir_aeha
